14af4cb15255203baac0edb91adaa298458cbbe4
Chloe Dawn <chloe@sapphic.dev>
2020-12-22 18:14:19 +0000

    Merge slow falling mixin with living entity mixin

--- src/main/java/dev/sapphic/beacons/mixin/LivingEntityMixin.java
+++ src/main/java/dev/sapphic/beacons/mixin/LivingEntityMixin.java
@@ -9,7 +9,9 @@ import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
@@ -20,16 +22,21 @@ abstract class LivingEntityMixin extends Entity {
     super(type, level);
   }
 
+  @Shadow
+  public abstract boolean hasEffect(final MobEffect effect);
+
   @Inject(method = "<init>", at = @At("RETURN"), require = 1, allow = 1)
   private void storeDefaultMaxStepUp(final CallbackInfo ci) {
     this.defaultMaxStepUp = this.maxUpStep;
   }
 
-  @Shadow
-  public abstract boolean hasEffect(final MobEffect effect);
-
   @Inject(method = "tickEffects", at = @At("HEAD"), require = 1, allow = 1)
   private void updateJumpBoostStepAssist(final CallbackInfo ci) {
     this.maxUpStep = this.hasEffect(MobEffects.JUMP) ? 1.0F : this.defaultMaxStepUp;
+  }
+
+  @ModifyConstant(method = "travel", constant = @Constant(doubleValue = 0.01, ordinal = 0), require = 1, allow = 1)
+  private double dropIfCrouching(final double fallDelta) {
+    return (this.hasEffect(MobEffects.SLOW_FALLING) && !this.isCrouching()) ? fallDelta : 0.08;
   }
 }
