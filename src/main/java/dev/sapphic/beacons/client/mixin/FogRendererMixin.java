fa079b7fdb590ad04c2b85e49b4714ee7038f080
Chloe Dawn <chloe@sapphic.dev>
2021-01-21 18:42:36 +0000

    Shift injection point before invocation arguments

--- src/main/java/dev/sapphic/beacons/client/mixin/FogRendererMixin.java
+++ src/main/java/dev/sapphic/beacons/client/mixin/FogRendererMixin.java
@@ -1,25 +1,40 @@
 package dev.sapphic.beacons.client.mixin;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.Camera;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.FogRenderer;
-import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import org.jetbrains.annotations.Nullable;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.At.Shift;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
 @Mixin(FogRenderer.class)
 abstract class FogRendererMixin {
-  @Redirect(method = "setupColor", at = @At(value = "INVOKE",
-    target = "Lnet/minecraft/world/entity/LivingEntity;hasEffect(Lnet/minecraft/world/effect/MobEffect;)Z",
-    ordinal = 1), require = 1, allow = 1)
-  private static boolean hasOnlyBaseNightVision(final LivingEntity entity, final MobEffect effect) {
-    final @Nullable MobEffectInstance nightVision = entity.getEffect(effect);
+  @Shadow private static float fogRed;
+  @Shadow private static float fogGreen;
+  @Shadow private static float fogBlue;
 
-    return (nightVision != null) && (nightVision.getAmplifier() < 1);
+  @Inject(method = "setupColor(Lnet/minecraft/client/Camera;FLnet/minecraft/client/multiplayer/ClientLevel;IF)V",
+    require = 1, allow = 1, cancellable = true,
+    at = @At(shift = Shift.BY, by = -4, value = "INVOKE", opcode = Opcodes.INVOKESTATIC,
+      target = "Lnet/minecraft/client/renderer/GameRenderer;getNightVisionScale(Lnet/minecraft/world/entity/LivingEntity;F)F"))
+  private static void skipNightVisionColorShift(final Camera camera, final float tickDelta, final ClientLevel level, final int renderDistance, final float shade, final CallbackInfo info) {
+    final @Nullable MobEffectInstance nightVision = ((LivingEntity) camera.getEntity()).getEffect(MobEffects.NIGHT_VISION);
+
+    if ((nightVision != null) && (nightVision.getAmplifier() > 0)) {
+      RenderSystem.clearColor(fogRed, fogGreen, fogBlue, 0.0F);
+      info.cancel();
+    }
   }
 }
