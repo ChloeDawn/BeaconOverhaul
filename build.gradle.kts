102b59bffebd8c1cbc84def2f14e11b44da41cc8
Chloe Dawn <chloe@sapphic.dev>
2021-06-07 17:50:09 +0100

    Throw exception if keystore is missing

--- build.gradle.kts
+++ build.gradle.kts
@@ -114,6 +114,10 @@ if (hasProperty("signing.mods.keyalias")) {
   val keystore = property("signing.mods.keystore")
   val password = property("signing.mods.password")
 
+  if (!project.file(keystore!!).exists()) {
+    error("Missing keystore $keystore")
+  }
+
   listOf(tasks.remapJar, tasks.remapSourcesJar).forEach {
     it.get().doLast {
       val file = outputs.files.singleFile
