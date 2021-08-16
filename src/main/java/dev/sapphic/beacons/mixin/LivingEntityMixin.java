a7b50a2844a2118d6ce8bf662481a94a84d1d0fb
Chloe Dawn <chloe@sapphic.dev>
2021-08-16 20:38:30 +0100

    Make slow fall dropping mixin less intrusive

--- src/main/java/dev/sapphic/beacons/mixin/LivingEntityMixin.java
+++ src/main/java/dev/sapphic/beacons/mixin/LivingEntityMixin.java
@@ -6,13 +6,13 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
@@ -47,10 +47,12 @@ abstract class LivingEntityMixin extends Entity {
     }
   }
 
-  @ModifyConstant(
+  @ModifyVariable(
     method = "travel(Lnet/minecraft/world/phys/Vec3;)V",
-    require = 1, allow = 1,
-    constant = @Constant(doubleValue = 0.01, ordinal = 0))
+    at = @At(
+      shift = At.Shift.BEFORE, value = "FIELD", opcode = Opcodes.PUTFIELD, ordinal = 0,
+      target = "Lnet/minecraft/world/entity/LivingEntity;fallDistance:F"), 
+    require = 1, allow = 1)
   private double dropIfCrouching(final double fallDelta) {
     return (this.hasEffect(MobEffects.SLOW_FALLING) && !this.isCrouching()) ? fallDelta : 0.08;
   }
