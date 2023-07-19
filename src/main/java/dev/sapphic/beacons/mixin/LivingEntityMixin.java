b6e2d5f6f1194c30584bba5c17aeee360eaaddb5
Chloe Dawn <chloe@sapphic.dev>
2023-07-19 14:56:39 +0100

    Fix preemptive initialization errors

--- src/main/java/dev/sapphic/beacons/mixin/LivingEntityMixin.java
+++ src/main/java/dev/sapphic/beacons/mixin/LivingEntityMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
+import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -17,7 +18,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(LivingEntity.class)
 abstract class LivingEntityMixin extends Entity {
   @Unique
-  private float defaultStepHeight;
+  private @MonotonicNonNull Float baseUpStep;
 
   @Unique
   private boolean stepIncreased;
@@ -26,11 +27,11 @@ abstract class LivingEntityMixin extends Entity {
     super(type, level);
   }
 
-  @Inject(
-      method = "<init>(Lnet/minecraft/world/entity/EntityType;Lnet/minecraft/world/level/Level;)V",
-      require = 1, allow = 1, at = @At("RETURN"))
-  private void setDefaultStepHeight(final CallbackInfo ci) {
-    this.defaultStepHeight = this.maxUpStep();
+  @Inject(method = "baseTick()V", at = @At("HEAD"), require = 1)
+  private void setBaseUpStep(final CallbackInfo ci) {
+    if (this.baseUpStep == null) {
+      this.baseUpStep = this.maxUpStep();
+    }
   }
 
   @Shadow
@@ -44,7 +45,7 @@ abstract class LivingEntityMixin extends Entity {
         this.stepIncreased = true;
       }
     } else if (this.stepIncreased) {
-      this.setMaxUpStep(this.defaultStepHeight);
+      this.setMaxUpStep(this.baseUpStep);
       this.stepIncreased = false;
     }
   }
